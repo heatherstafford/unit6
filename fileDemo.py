2b97448bee09dca92f8e3c594c4090736d6660d7
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-09 08:38:17 -0400

    Updated from Brython Server: 5/9/2018 8:38:17 AM

--- fileDemo.py
+++ fileDemo.py
@@ -7,8 +7,8 @@ file = open('engmix.txt')
 numWords = 0
 
 for line in file:
-    if 'smed' in line:
-        print(line)
+    if 'heath' in line:
+        print(line.strip())
     numWords += 1
     
 print(numWords)
