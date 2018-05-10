096e0b9a46ed1e1dd0a4052967671cd49e7f4bf5
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-10 08:20:23 -0400

    Updated from Brython Server: 5/10/2018 8:20:22 AM

--- zzwords.py
+++ zzwords.py
@@ -1,1 +1,14 @@
+#Heather Stafford
+#5/10/18
+#zzwords.py
 
+file = open('engmix.txt')
+
+numWords = 0
+
+for line in file:
+    if 'zz' in line:
+        print(line.strip())
+    numWords += 1
+    
+print(numWords)
