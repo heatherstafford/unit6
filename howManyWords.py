5694b0c34cf22690b303c25a023b688c138cfd71
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-10 08:45:14 -0400

    Updated from Brython Server: 5/10/2018 8:45:13 AM

--- howManyWords.py
+++ howManyWords.py
@@ -7,7 +7,10 @@ file = open('engmix.txt')
 L = [0]*22
 
 for line in file:
-    L[len(line.strip())-1] += 1
+    if (len(line.strip())-1) > 0:
+        L[len(line.strip())-1] += 1
+    else:
+        L[len(line.split())-1
     
 for i in range(0,22):
     print(i,'letter words:', L[i - 1])
