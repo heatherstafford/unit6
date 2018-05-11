0c96b8289b5e53a879a7230738526ff9f487d225
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-11 08:42:27 -0400

    Update warmup16.py

--- warmup16.py
+++ warmup16.py
@@ -4,7 +4,12 @@
 
 file = open('engmix.txt')
 
+numwords = 0
+
 for line in file:
     word = line.strip()
     if len(word)>0 and word[0] == 'h' and word[-1] == 's':
         print(word)
+        numwords += 1
+    
+print(numwords)
