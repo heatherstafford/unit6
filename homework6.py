2416c2cd5267964cc8ad56a5fc283549753ad9ac
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-14 08:05:30 -0400

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -1,1 +1,10 @@
 
+
+file = open('engmix.txt')
+
+word = input('Enter a word: ')
+
+if word in file:
+    print('The word is in the dictionary')
+else:
+    print('The word is not in the dictionary')
