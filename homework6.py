90faa9ff47df9a4ff3636b9b746d772805242e31
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-14 08:23:11 -0400

    Updated from Brython Server: 5/14/2018 8:23:11 AM

--- homework6.py
+++ homework6.py
@@ -2,6 +2,7 @@
 #5/14/18
 #homework6.py - how to struggle
 
+"""
 file = open('engmix.txt')
 
 word = input('Enter a word: ')
@@ -13,3 +14,12 @@ for line in file:
         result = 'is'
         
 print('The word', result, 'in the dictionary')
+"""
+
+file = open('engmix.txt')
+
+L = file
+
+num = input('Enter a number: ')
+
+print(L[num]
