6d285f466e243ff21572c966b0a9c71006fdb712
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-14 08:14:54 -0400

    Updated from Brython Server: 5/14/2018 8:14:53 AM

--- homework6.py
+++ homework6.py
@@ -5,9 +5,7 @@ file = open('engmix.txt')
 word = input('Enter a word: ')
 
 for line in file:
-    if word == line:
+    if word == line.strip():
         result = 'is'
-    else:
-        result = 'is not'
         
 print('The word', result, 'in the dictionary')
