1ae3b7572784f8876263538f4f3bf4fbc2a8a578
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-17 08:14:16 -0400

    Updated from Brython Server: 5/17/2018 8:14:15 AM

--- warmup17.py
+++ warmup17.py
@@ -2,7 +2,7 @@
 #5/17/18
 #warmup17.py
 
-file = open('engmix.py')
+file = open('engmix.txt')
 
 for line in file:
     if 's' in line and 't' in line and 'a' in line and 'f' in line and 'o' in line and 'r' in line and 'd' in line:
