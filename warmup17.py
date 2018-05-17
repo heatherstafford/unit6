e20ad6d19edcc6d696d20da7685ff2392a1d2b84
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-17 08:12:27 -0400

    Updated from Brython Server: 5/17/2018 8:12:26 AM

--- warmup17.py
+++ warmup17.py
@@ -1,1 +1,9 @@
+#Heather Stafford
+#5/17/18
+#warmup17.py
 
+file = open('engmix.py')
+
+for line in file:
+    if 's' in line and 't' in line and 'a' in line and 'f' in line and 'o' in line and 'r' in line and 'd' in line:
+        print(line.strip())
