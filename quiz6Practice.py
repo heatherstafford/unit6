0d09d0463ebf8e449324ee2872946ef1779655f9
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-18 08:35:41 -0400

    Updated from Brython Server: 5/18/2018 8:35:40 AM

--- quiz6Practice.py
+++ quiz6Practice.py
@@ -1,1 +1,15 @@
+#Heather Stafford
+#5/18/18
+#quiz6Practice.py
 
+#program 1
+file = open('engmix.txt')
+
+"
+for line in file:
+    word = line.lower()
+    letterC = int(word.count('c'))
+    letterP = int(word.count('p'))
+    if letterC == 3 and letterP == 2:
+        print(word)
+"    
