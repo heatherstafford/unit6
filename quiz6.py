6cd625acfc04515337045db323f43f6a3adca831
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-21 08:04:24 -0400

    Updated from Brython Server: 5/21/2018 8:04:23 AM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,13 @@
+#Heather Stafford
+#5/21/18
+#quiz6.py
 
+file = open('engmix.txt')
+
+#Program #1
+letter = input('Enter a letter: ')
+
+for line in file:
+    if line.count(letter) >= 4:
+        print(line.strip())
+    
