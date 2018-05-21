96ea12e44cfb892e44cb82eac59ef4ba3e818b08
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-21 08:15:33 -0400

    Updated from Brython Server: 5/21/2018 8:15:32 AM

--- quiz6.py
+++ quiz6.py
@@ -12,8 +12,6 @@ for line in file:
     if line.count(letter1) >= 4:
         print(line.strip())
     
-"""
-    
 #Program #3
 number3 = int(input('Enter a number: '))
 letter3 = input('Enter a letter: ')
@@ -23,3 +21,18 @@ for line in file:
     if len(word)>0:
         if word[0] == letter3 and len(word) == number3:
             print(word)
+"""
+
+#Program #5
+
+for line in file:
+    vowelnumber = 0
+    avowel = line.count('a')
+    evowel = line.count('e')
+    ivowel = line.count('i')
+    ovowel = line.count('o')
+    uvowel = line.count('u')
+    newvowels = avowel + evowel + ivowel + ovowel + uvowel
+    if newvowels > vowelnumbers:
+        vowelnumbers += newvowels
+    
